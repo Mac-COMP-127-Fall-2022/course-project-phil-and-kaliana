b773280ea1557cb76744fd409a325ff67d213bf9
monsieurjonees <phil.reitzjones@me.com>
2022-11-19 16:19:44 -0600

    UI implementation started; constants added

--- src/Werdill/WerdillGame.java
+++ src/Werdill/WerdillGame.java
@@ -5,22 +5,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import edu.macalester.graphics.CanvasWindow;
+
 public class WerdillGame {
     private static final String WORD_LIST_PATH = "res/wordList/filteredWords.txt";
 
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 800;
+
     private final List<String> words;
     private final WerdillUI ui;
-    private final Checker checker;
+    // private final Checker checker; // commented out during UI dev
     
     WerdillGame() throws IOException {
         words = Files.lines(Path.of(WORD_LIST_PATH)).toList();
 
         ui = new WerdillUI(this);
-        checker = new Checker(chooseSolution());
+        // checker = new Checker(chooseSolution());
         
-        CanvasWindow canvas = new CanvasWindow();
+        CanvasWindow canvas = new CanvasWindow("Werdill", CANVAS_HEIGHT, CANVAS_WIDTH);
         canvas.add(ui);
-
     }
 
     private String chooseSolution() {
@@ -31,7 +35,7 @@ public class WerdillGame {
         return words;
     }
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        new WerdillGame();
     }
 }
