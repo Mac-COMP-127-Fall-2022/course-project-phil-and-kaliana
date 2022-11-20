15df527788dac75247125aed0d6122eb45a51878
monsieurjonees <phil.reitzjones@me.com>
2022-11-20 17:47:58 -0600

    Refactor all check-related methods to Checker

--- src/Werdill/Checker.java
+++ src/Werdill/Checker.java
@@ -1,9 +1,19 @@
 package Werdill;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Random;
 
 public class Checker {
     private String[] solution;
+
+    private final List<String> words;
+    private final List<String> solutions;
+
+    private static final String WORD_LIST_PATH = "res/wordList/words.txt";
+    private static final String SOLUTIONS_PATH = "res/wordList/solutions.txt";
 
     private Random rand = new Random();
 
@@ -11,16 +21,31 @@ public class Checker {
         return solution.toString();
     }
 
-    public void setSolution(String newSolution) {
+    public void setSolution(String newSolution) { // NOTE: switch to private if not unit testing
+        // System.out.println(newSolution);
         solution = newSolution.toUpperCase().split("");
     }
 
-    public Checker(String solution) {
-        setSolution(solution);
+    public Checker() throws IOException {
+        words = Files.lines(Path.of(WORD_LIST_PATH)).toList();
+        solutions = Files.lines(Path.of(SOLUTIONS_PATH)).toList();
+
+        chooseSolution();
     }
 
-    public int[] check(String[] guess) {
-        int[] ret = new int[5];
+    private void chooseSolution() {
+        int index = rand.nextInt(solutions.size());
+        setSolution(solutions.get(index));
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+
+    public Integer[] check(String[] guess) {
+        //TODO: Check is in word list
+
+        Integer[] ret = new Integer[5];
         // <testVersion>
         for (int i = 0; i < 5; i++) {
             ret[i] = rand.nextInt(3);
--- src/Werdill/WerdillGame.java
+++ src/Werdill/WerdillGame.java
@@ -1,41 +1,29 @@
 package Werdill;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
+// import java.util.Random;
 
 import edu.macalester.graphics.CanvasWindow;
 
 public class WerdillGame {
-    private static final String WORD_LIST_PATH = "res/wordList/filteredWords.txt";
-
     private static final int CANVAS_WIDTH = 600;
     private static final int CANVAS_HEIGHT = 800;
 
-    private final List<String> words;
     private final WerdillUI ui;
     private final Checker checker;
+
+    // private final Random rand = new Random();
 
     private final CanvasWindow canvas;
 
     WerdillGame() throws IOException {
-        words = Files.lines(Path.of(WORD_LIST_PATH)).toList();
 
-        checker = new Checker(chooseSolution());
+        checker = new Checker();
 
         canvas = new CanvasWindow("Werdill", CANVAS_HEIGHT, CANVAS_WIDTH);
         ui = new WerdillUI(canvas, checker);
         
         canvas.add(ui);
-    }
-
-    private String chooseSolution() {
-        return "world";
-    }
-
-    public List<String> getWords() {
-        return words;
     }
     
     public static void main(String[] args) throws IOException {
