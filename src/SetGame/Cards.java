eec5ed45d0ee01503fcfb82cf04e6f386e085dc8
Kaliana Andriamananjara <kandriam@macalester.edu>
2022-12-04 12:36:36 -0600

    Working on Cards/Deck... owie

--- src/SetGame/Cards.java
+++ src/SetGame/Cards.java
@@ -1,26 +1,39 @@
 package SetGame;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Cards {
-    private List<Integer> Colors;
-    private List<Integer> Nums;
-    private List<Integer> Shapes;
-    private List<Integer> Fills;
+    private Integer color;
+    private Integer num;
+    private Integer shape;
+    private Integer fill;
 
-    public Deck = createDeck();
+    public Cards(Integer color, Integer num, Integer shape, Integer fill) {
+        this.color = color;
+        this.num = num;
+        this.shape = shape;
+        this.fill = fill;
 
-    List<Cards> CreateDeck() {
-        ArrayList<Integer> cardDeck = new ArrayList<>();
-        for (Integer c = 1; c <= 3; c++) {
-            for (Integer n = 1; n <= 3; n++) {
-                for (Integer s = 1; s <= 3; s++) {
-                    for (Integer f = 1; f <= 3; f++) {
-                        Cards card = Cards()
+    }
+
+
+    List<Cards> Deck = new ArrayList<>();
+
+
+    public List<Cards> CreateDeck() {
+        List<Cards> cardDeck = new ArrayList<>();
+        for (int c = 0; c < 3; c++) {
+            for (int n = 0; n < 3; n++) {
+                for (int s = 0; s < 3; s++) {
+                    for (int f = 0; f < 3; f++) {
+                        cardDeck.add(Cards(c, n, s, f)); // ????????????????????
                     }
                 }
             }
         }
+        return cardDeck;
     }
+
 }
